a583d976a1917ede695d97163ade09f150a35a3b
Deshani <deshani.ranasingha@qualitapps.com>
2021-03-07 10:20:06 +0530

    add git-hub url for WebView

--- lib/app/global/global.dart
+++ lib/app/global/global.dart
@@ -1,10 +1,12 @@
 library my_prj.global;
 import 'package:intl/intl.dart';
 
-String url = '';
 
 var format = DateFormat('MMM d');
 var timeFormat = DateFormat('hh:mm a');
+
 String displayDate;
 String formatDate;
 String formatTime;
+
+String github_url = "https://github.com/DeshaniRanasinga28/vdev-20210305/tree/ui-dev";
--- lib/app/ui/screen/info/project_screen.dart
+++ lib/app/ui/screen/info/project_screen.dart
@@ -1,5 +1,12 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:vdev/app/global/app_colors.dart';
+import 'package:vdev/app/global/global.dart' as global;
+import 'package:webview_flutter/webview_flutter.dart';
+
+
 
 class ProjectScreen extends StatefulWidget {
 
@@ -12,19 +19,23 @@ class ProjectScreen extends StatefulWidget {
 class _ProjectScreenState extends State<ProjectScreen>{
 
   @override
-  Widget build(BuildContext context) {
-    var w = MediaQuery.of(context).size.width;
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: white225,
-      body: SafeArea(
-          child: Column(
-            children: [
-              Text("_ProjectScreenState")
-            ],
-          )
-      ),
-    );
+        body: WebView(
+          gestureRecognizers: [
+            Factory<VerticalDragGestureRecognizer>(
+                    () => VerticalDragGestureRecognizer())].toSet(),
+          initialUrl: global.github_url,
+          javascriptMode: JavascriptMode.unrestricted,
+        ));
   }
 
 }
